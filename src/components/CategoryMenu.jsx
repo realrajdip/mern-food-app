97595e7828f6ea03d369d6b7b9ffbabf8fdb0577
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-05-27 10:01:23 +0530

    error fixed

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import FoodData from "../data/FoodData.js";
 import { useDispatch, useSelector } from "react-redux";
-import { setCategory } from '../redux/CategorySlice.js';
+import { setCategory } from "../redux/CategorySlice.js";
 
 const CategoryMenu = () => {
   const [categories, setCategories] = useState([]);
   const uniqueListCategories = () => {
     const uniqueCategories = [
       ...new Set(FoodData.map((food) => food.category)),
-    ]; 
+    ];
     setCategories(() => uniqueCategories);
-    console.log(categories)
+    console.log(categories);
   };
 
   useEffect(() => {
@@ -18,22 +18,26 @@ const CategoryMenu = () => {
   }, []);
 
   const dispatch = useDispatch();
-  const selectedCategory =  useSelector(state => state.category.category)
+  const selectedCategory = useSelector((state) => state.category.category);
   return (
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
-      <button
-              onClick={() => dispatch(setCategory("All"))}
-              className={`px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white ${selectedCategory === "All" && "bg-green-500 text-white"}`}
-            >
-              All
-            </button>
+        <button
+          onClick={() => dispatch(setCategory("All"))}
+          className={`px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white ${
+            selectedCategory === "All" && "bg-green-500 text-white"
+          }`}
+        >
+          All
+        </button>
         {categories.map((category, index) => {
           return (
             <button
               onClick={() => dispatch(setCategory(category))}
-              className={`px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white ${selectedCategory === category && "bg-green-500 text-white"} `}
+              className={`px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white ${
+                selectedCategory === category && "bg-green-500 text-white"
+              } `}
               key={index}
             >
               {category}
