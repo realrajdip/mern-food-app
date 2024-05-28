574037274604d03527037fb8db54051f064d120e
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-05-28 22:04:34 +0530

    new feature

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -23,8 +23,9 @@ const CategoryMenu = () => {
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
+        {/* for all tag */}
         <button
-          onClick={() => dispatch(setCategory("All"))}
+          onClick={() => dispatch(setCategory("All"))}   
           className={`px-3 py-2 bg-gray-200 font-bold rounded-md hover:bg-green-500 hover:text-white ${
             selectedCategory === "All" && "bg-green-500 text-white"
           }`}
