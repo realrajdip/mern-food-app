39aed9b10609c6d89d445437dad5ce174b99d049
Rajdip Bandopadhyay <rajdipbandopadhyay100@gmail.com>
2024-05-29 20:12:43 +0530

    new feature

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -20,6 +20,7 @@ const CategoryMenu = () => {
   const dispatch = useDispatch();
   const selectedCategory = useSelector((state) => state.category.category);
   return (
+    // adding style for categorymenu
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
